42647bd526cb310fd8d05c0bbc925c671fb2ef3b
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2024-12-16 08:16:17 +0530

    Fix deprecation warning

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -137,7 +137,7 @@ void setup() {
   esp_adc_cal_characteristics_t adc_chars;
   esp_adc_cal_value_t val_type =
       esp_adc_cal_characterize(ADC_UNIT_1,
-                               ADC_ATTEN_DB_11,
+                               ADC_ATTEN_DB_12,
                                ADC_WIDTH_BIT_12,
                                1100,
                                &adc_chars);  // Check type of calibration value used to characterize ADC
